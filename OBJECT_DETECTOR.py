d05cd9d379d1e8ac1df00621ad17a0f239a07702
EMRE YÜZSÜREN <emreyuzsuren@hotmail.com>
2026-04-30 14:01:53 +0300

    initial commit

---
+++ OBJECT_DETECTOR.py
@@ -0,0 +1,17 @@
+import numpy as np
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+IMAGE_FILE = 'face-1.jpg'
+
+import cv2
+
+img = cv2.imread(IMAGE_FILE)
+if img is None:
+    raise FileNotFoundError(f"Could not read image file: {IMAGE_FILE}")
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
